0adcf78a396e5331be58a7d9d0362770cdc26d3a
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-04-22 09:55:20 -0400

    problemas no env: mudei o APP_URL para funcionar

--- dist/config/appConfig.js
+++ dist/config/appConfig.js
@@ -1,4 +1,4 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = {
-  url: 'http://localhost:3001', // porta 80
+  url: 'http://192.168.100.200:3001', // porta 80
   // url: 'http://localhost:3001',
 };
--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,4 +1,4 @@
 export default {
-  url: 'http://localhost:3001', // porta 80
+  url: 'http://192.168.100.200:3001', // porta 80
   // url: 'http://localhost:3001',
 };
